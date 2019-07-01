52ec92849b1544766125c2e236599c72376e988a
Sanat <taorisanat@gmail.com>
2019-07-01 18:26:29 +0530

    Search feature implemented

--- src/app/event/components/main-content/main-content.component.ts
+++ src/app/event/components/main-content/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 // load json file locally
 import SampleJson from '../../../../assets/data.json';
@@ -6,11 +6,14 @@ import SampleJson from '../../../../assets/data.json';
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
-  styleUrls: ['./main-content.component.css']
+  styleUrls: ['./main-content.component.scss']
 })
 
 export class MainContentComponent implements OnInit {
   events:any;
+
+  public EventFilter: string;
+  @Output() toggleSideNav = new EventEmitter<void>();
 
   constructor() {
     console.log(SampleJson);
